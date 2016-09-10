f5a79b07d1604795015d7704a1b3e0df2c094284
hirokisan <expsharetoworld@gmail.com>
2016-09-10 20:38:02 +0900

    メニューバー、スクロール、バックグラウンドスクロール拒否
    アニメーションをつける

--- sp/index.php
+++ sp/index.php
@@ -4,231 +4,182 @@
 	<meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=10.0, user-scalable=yes">
 <link rel="stylesheet" href="../css/reset.css" media="all">
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 	<title></title>
 </head>
 <body>
     <div class="width100per">
         <div class="relative">
             <h1 class="">this is mobile site</h1>
-            <p class="absolute">CV</p>
+            <button class="absolute" id="accordion-menu">CV</button>
+        </div>
+        <div id="accordion" class="display_off">
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+    <p>hello mobile</p>
+
         </div>
     </div>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
-	<p>hello mobile</p>
+	<div class="contents">
+        <p>背景の位置が0に戻ってしまうのをなんとかしたい</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	    <p>hello mobile</p>
+	</div>
 </body>
 </html>
 
 <style>
 .width100per {
     position: fixed !important;
-    position: absolute;
     top: 0;
     left: 0;
     width: 100%;
+    height: 50px;
     line-height: 50px;
     background-color: #4E9ABE;
     color: #fff;
@@ -262,4 +213,52 @@
     bottom: 0px;
     right: 15px;
 }
+.display_off{
+    display: none;
+}
+.contents {
+    margin-top: 80px;
+}
+body {
+  width: 100%;
+}
+
+body.noscroll {
+  position: fixed;
+  overflow-y: scroll;
+}
+.fixed {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+}
+.open {
+    opacity: 1;
+    display: block;
+    height: 300px;
+    background: green;
+    overflow-y: scroll;
+    z-index: 10;
+}
 </style>
+<script>
+$(function(){
+    var state = false;
+    var scrollpos;
+
+    $('#accordion-menu').on('click', function(){
+        if(state == false) {
+            scrollpos = $(window).scrollTop();
+            $('body').addClass('fixed').css({'top': -scrollpos});
+            $('#accordion').addClass('open');
+            state = true;
+        } else {
+            $('body').removeClass('fixed').css({'top': 0});
+            window.scrollTo( 0 , scrollpos );
+            $('#accordion').removeClass('open');
+            state = false;
+        }
+    });
+
+});
+</script>
